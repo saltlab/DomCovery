786a7d00e2bc03b3f3c8fcd5ce46a336db643c0c
mehdimir <maghaei@gmail.com>
2013-07-23 09:25:28 -0700

    relative location

--- src/test/java/salt/domcoverage/casestudies/photogallery/PhotogalleryTestSourceLocations.java
+++ src/test/java/salt/domcoverage/casestudies/photogallery/PhotogalleryTestSourceLocations.java
@@ -1,9 +1,9 @@
 package salt.domcoverage.casestudies.photogallery;
 
 public class PhotogalleryTestSourceLocations {
-	
-	public static String TestAddCategory = "/Users/mehdimir/git/DomCoverage/src/main/java/salt/domcoverage/casestudies/photogallery/Add_category.java";
-	public static String TestSlideShow = "/Users/mehdimir/Desktop/Dropbox/UBC-research/development/eclipse-workspace/CaseStudySubjectsTests/src/main/java/org/photogallery/SlideShowPauseTest.java";
-	public static String TestEnteringName = "/Users/mehdimir/Desktop/Dropbox/UBC-research/development/eclipse-workspace/CaseStudySubjectsTests/src/main/java/org/photogallery/TestEnteringName.java";
+	static String userDir=System.getProperty("user.dir");
+	public static String TestAddCategory = userDir+ "/src/main/java/salt/domcoverage/casestudies/photogallery/Add_category.java";
+	public static String TestSlideShow = userDir+"/src/main/java/org/photogallery/SlideShowPauseTest.java";
+	public static String TestEnteringName = userDir+"/src/main/java/org/photogallery/TestEnteringName.java";
 
 }
